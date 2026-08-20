7c5f684a3b2aacb23ea7dbd96dbba0e0f6b1cb03
IdushaGaravi <idushagaravi@gmail.com>
2026-08-20 10:22:34 +0530

    implement pending requests ui

--- pos-frontend/pos-frontend/src/pages/branch/Inventory/InventoryTable.jsx
+++ pos-frontend/pos-frontend/src/pages/branch/Inventory/InventoryTable.jsx
@@ -14,31 +14,31 @@ const inventories = [
 const InventoryTable = ({setIsEditDialogOpen}) => {
   return (
     <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="">SKU</TableHead>
-                  <TableHead className="">Product Name</TableHead>
-                  <TableHead className="">Quantity</TableHead>
-                  <TableHead className="text-right">Category</TableHead>
-                  <TableHead className="text-right">Action</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {inventories.map((item) => (
-                  <TableRow key={item.id}>
-                    <TableCell>{item.sku}</TableCell>
-                    <TableCell>{item.name}</TableCell>
-                    <TableCell>{item.quantity}</TableCell>
-                    <TableCell className="text-right">{item.category}</TableCell>
-                    <TableCell className="text-right">
-                        <Button onClick={() => setIsEditDialogOpen(true)} variant="outline" size="icon">
-                            <Edit />
-                        </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="">SKU</TableHead>
+          <TableHead className="">Product Name</TableHead>
+          <TableHead className="">Quantity</TableHead>
+          <TableHead className="text-right">Category</TableHead>
+          <TableHead className="text-right">Action</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {inventories.map((item) => (
+          <TableRow key={item.id}>
+            <TableCell>{item.sku}</TableCell>
+            <TableCell>{item.name}</TableCell>
+            <TableCell>{item.quantity}</TableCell>
+            <TableCell className="text-right">{item.category}</TableCell>
+            <TableCell className="text-right">
+                <Button onClick={() => setIsEditDialogOpen(true)} variant="outline" size="icon">
+                    <Edit />
+                </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
 
--- pos-frontend/pos-frontend/src/pages/superAdmin/PendingRequest.jsx
+++ pos-frontend/pos-frontend/src/pages/superAdmin/PendingRequest.jsx
@@ -1,6 +1,71 @@
+import { Info, Edit, Check, X } from "lucide-react"
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent } from "@/components/ui/card";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { Button } from "@/components/ui/button"
+
+const stores = [1]
+
 const PendingRequest = () => {
+  const handleAccept = () => {
+    console.log("Handle accept")
+  }
+  
+  const handleReject = () => {
+    console.log("Handle reject")
+  }
+
   return (
-    <div>PendingRequest</div>
+    <div className="space-y-6">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl font-bold tracking-tight">Pending Requests</h1>
+
+        <div className="flex gap-2">
+          <Badge className='gap-2'><Info />Pending</Badge>
+        </div>
+      </div>
+      
+      <Card>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="">Store Name</TableHead>
+                <TableHead className="">Owner</TableHead>
+                <TableHead className="">Contact</TableHead>
+                <TableHead className="">Business Type</TableHead>
+                <TableHead className="">Submitted On</TableHead>
+                <TableHead className="text-right">Action</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {stores.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell>{item.sku}</TableCell>
+                  <TableCell>{item.name}</TableCell>
+                  <TableCell>{item.quantity}</TableCell>
+                  <TableCell className="">{item.category}</TableCell>
+                  <TableCell className="">{item.category}</TableCell>
+                  <TableCell className="text-right">
+                    <div className="flex justify-end items-center gap-2">
+                      <Button onClick={handleAccept} variant="outline" className="bg-green-100">
+                          <Check />
+                          Accept
+                      </Button>
+
+                      <Button onClick={handleReject} variant="outline" className="bg-red-100">
+                          <X />
+                          Reject
+                      </Button>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </div> 
   )
 }
 
